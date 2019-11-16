f1d8928c8bd0601829f6f032649fbe6f0e719d78
Luis Hernandez Ayala <luishernandezayala@MacBook-Pro-de-Luis-2.local>
2019-11-16 10:09:31 -0600

    Gaussy value should be multiplied in the fussy network, not used as x

--- GaussyModel/GaussyModel.py
+++ GaussyModel/GaussyModel.py
@@ -18,16 +18,16 @@ except:
 logger = open('logs.log', 'w')
 
 class GaussyModel():
-    def __init__(self, variables, x_step, bottom_limit=-200, top_limit=500):
+    def __init__(self, variables, x_step, bottom_limit, top_limit):
         self.variables = variables
         self.x_step = x_step
         self.bottom_limit = bottom_limit
         self.top_limit = top_limit
+        self.range = top_limit - bottom_limit
 
+        for element in variables:
+            element['gaussy_values'] = [0] * self.range
 
-    @staticmethod
-    def evaluate_x(x, median, desv_std):
-        return math.e ** ((-0.5) * (((x-median) / desv_std) ** 2))
 
     def log(self, text, debuglevel=0, logtype="INFO"):
         if self.debuglevel <= debuglevel:
@@ -35,76 +35,64 @@ class GaussyModel():
             print(msg)
             logger.write(msg + '\n')
 
-
-    def plot_ranges(self, plt):        
-        for temp in self.variables:
-            values = self.get_function_values(temp)
-
-            if not plt_found:
-                print("Matplotlib libary not found. Install it to see the graph. Install: 'pip install matplotlib'")
-                return
-
-            plt.plot([i*self.x_step for i in range(self.bottom_limit, self.top_limit)], values)
-
-        
-        plt.set_xlabel('Temperature')
-        plt.set_ylabel('Membership Grade')
-        plt.grid(True)
+    @staticmethod
+    def evaluate_x(x, median, desv_std):
+        return math.e ** ((-0.5) * (((x-median) / desv_std) ** 2))
 
 
-    def get_function_values(self, variable):
-        desv_std = variable['desv_std']
-        median = variable['median']
-
-        result = [0] * (self.top_limit - self.bottom_limit)
-        for i in range(self.bottom_limit, self.top_limit):
-            x = i*self.x_step
-            value = GaussyModel.evaluate_x(x, median, desv_std)
-
-            result[i-self.bottom_limit] = value
-
-        return result
+    @staticmethod
+    def evaluate_fussy(x, p, q, gaussy_value):
+        return gaussy_value * ((p * x) + q)
 
 
-    def get_fussy_values(self):
-        result = [0] * (self.top_limit - self.bottom_limit)
-        function_values = [0] * len(self.variables) 
-        processed_values = [0] * len(self.variables)
-
-        print(self.variables)
-
-        for i in range(self.bottom_limit, self.top_limit):
-            x = i*self.x_step
-            for j in range(len(self.variables)):
-                median = self.variables[j]['median']
-                desv_std = self.variables[j]['desv_std']
-                function_values[j] = GaussyModel.evaluate_x(x, median, desv_std)
-
-            for j in range(len(self.variables)):
-                p = self.variables[j]['p']
-                q = self.variables[j]['q']
-                processed_values[j] = p*function_values[j] + q
-
-            result[i-self.bottom_limit] = sum(processed_values) / sum(function_values)
-            
-        return result
-    
-
-    def plot_fussy_inference_network(self, plt):
-        values = self.get_fussy_values()
-        plt.plot([i*self.x_step for i in range(self.bottom_limit, self.top_limit)], values)
-        plt.set_xlabel('Temperature')
-        plt.set_ylabel('')
-        plt.grid(True)
-
-    def plot(self):
+    def config_plots(self):
         fig, axs = plt.subplots(1, 2)
         fig.figsize = (100,100)
-        # axs[0].title.set_text(f"Generation {generation}")
-        self.plot_fussy_inference_network(axs[0])
-        self.plot_ranges(axs[1])
+
+        axs[0].set_xlabel('Temperature')
+        axs[0].set_ylabel('Fussy Inference Network')
+        axs[0].grid(True)
+
+        axs[1].set_xlabel('Temperature')
+        axs[1].set_ylabel('Membership Grade')
+        axs[1].grid(True)
+
+        return fig, axs
+
+    def plot_ranges(self, plt, values):        
+        plt.plot([i*self.x_step for i in range(self.bottom_limit, self.top_limit)], values)
+
+
+    def plot(self):
+        fig, axs = self.config_plots()
+
+        fussy_network_values = [0] * self.range
+        for i in range(self.bottom_limit, self.top_limit):
+            x = i*self.x_step
+            gaussy_subtotal = 0
+            fussy_subtotal = 0
+
+            for element in self.variables:
+                gaussy_value = GaussyModel.evaluate_x(x, element['median'], element['desv_std'])
+                element['gaussy_values'][i-self.bottom_limit] = gaussy_value
+                gaussy_subtotal += gaussy_value
+
+                fussy_value = GaussyModel.evaluate_fussy(x, element['p'], element['q'], gaussy_value)
+                fussy_subtotal += fussy_value
+
+            fussy_network_values[i-self.bottom_limit] = fussy_subtotal / gaussy_subtotal
+
+            
+
+        # Plot temperature graph
+        for element in self.variables:
+            self.plot_ranges(axs[1], element['gaussy_values'])
+
+        # Plot fussy inference network
+        self.plot_ranges(axs[0], fussy_network_values)
 
         plt.show()
+        
 
     def fit(self):
         self.plot()
--- GaussyModel/main.py
+++ GaussyModel/main.py
@@ -6,24 +6,29 @@ def main():
             'median': 0,
             'desv_std': 6,
             'p': 0.04,
-            'q': 0
+            'q': 1,
+            'gaussy_values': None
         },
         {
             'median': 18,
             'desv_std': 6,
             'p': 0.05,
-            'q': 0
+            'q': 1,
+            'gaussy_values': None
         },
         {
             'median': 30,
             'desv_std': 6,
             'p': 0.08,
-            'q': 0
+            'q': 1,
+            'gaussy_values': None
         }
     ]
     tmpModel = GaussyModel(
                     variables=temperatures, 
-                    x_step=0.1)
+                    x_step=0.1,
+                    bottom_limit=-200,
+                    top_limit=500)
     tmpModel.fit()
 
 
